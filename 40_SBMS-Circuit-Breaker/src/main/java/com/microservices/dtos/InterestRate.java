0047dae8eced18d96be74d63a649c1c489665410
maruthicharanteja <124443822+maruthicharanteja10@users.noreply.github.com>
2024-08-13 22:55:29 +0530

    Add files via upload

---
+++ 40_SBMS-Circuit-Breaker/src/main/java/com/microservices/dtos/InterestRate.java
@@ -0,0 +1,14 @@
+package com.microservices.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InterestRate {
+    Integer id;
+    String type;
+    Double rateValue;
+}
